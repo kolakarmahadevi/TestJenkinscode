14c4f3aa4ba7b222d6a70f8ca2c6d4bc64080034
Lenovo\Windows 8.1 <mahadevi.kolakar@gamil.com>
2021-08-16 12:56:48 +0530

    Jenkins file added and tested the code

---
+++ src/testCode/Testingcode.java
@@ -0,0 +1,16 @@
+package testCode;
+
+public class Testingcode {
+
+	public static void main(String[] args) {
+		System.out.println("testin the  code");
+		
+		for(int i=1; i<5;i++)
+		{
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
